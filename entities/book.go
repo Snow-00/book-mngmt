b662cdabf15b43aa22bc3f5e735370f7d0fd596b
Snow-00 <wariorwilly43@gmail.com>
2023-08-24 23:22:00 +0700

    add router and configure new db

---
+++ entities/book.go
@@ -0,0 +1,7 @@
+package entities
+
+import "time"
+
+func Book struct {
+	ID 
+}
--- main.go
+++ main.go
@@ -5,12 +5,15 @@ import (
 	"log"
 	"net/http"
 	"fmt"
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	config.LoadConfig()
 	config.ConnectDB()
 
+	r := mux.NewRouter()
+
 	log.Println("Server is running on port", config.ENV.PORT)
-	http.ListenAndServe(fmt.Sprintf(":%v", config.ENV.PORT), nil)
+	http.ListenAndServe(fmt.Sprintf(":%v", config.ENV.PORT), r)
 }
