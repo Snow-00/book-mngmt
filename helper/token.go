e2581df6957be021f7267f41574d909609db86e3
Snow-00 <wariorwilly43@gmail.com>
2023-10-02 21:03:52 +0700

    even weirder (claims)

--- helper/token.go
+++ helper/token.go
@@ -8,14 +8,14 @@ import (
 	"github.com/Snow-00/book-mngmt/config"
 )
 
-var expDate = 2 * time.Minute
+// var expDate = 2 * time.Minute
 var mySigningKey = []byte(config.ENV.SECRET)
 
 type MyCustomClaims struct {
 	ID int `json:"id"`
 	Username string `json:"username"`
 	Role string `json:"role"`
-	jwt.ResgisterdClaims
+	jwt.RegisteredClaims
 }
 
 func CreateToken(user *entities.User) (string, error) {
@@ -23,8 +23,8 @@ func CreateToken(user *entities.User) (string, error) {
 		ID: user.ID,
 		Username: user.Username,
 		Role: user.Role,
-		jwt.ResgisterdClaims{
-			ExpiresAt: jwt.NewNumericDate(time.Now().Add(expDate)),
+		jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(2 * time.Minute)),
 			IssuedAt: jwt.NewNumericDate(time.Now()),
 			NotBefore: jwt.NewNumericDate(time.Now()),
 		},
